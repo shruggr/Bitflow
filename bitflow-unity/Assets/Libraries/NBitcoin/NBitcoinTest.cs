dc0d5aadcd889dcdd43b32f890183fb9a247e50f
Abel Dantas <dantas.abel@gmail.com>
2019-05-04 14:17:31 +0100

    Add NBitcoin
    https://programmingblockchain.gitbook.io/

---
+++ bitflow-unity/Assets/Libraries/NBitcoin/NBitcoinTest.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using NBitcoin;
+
+public class NBitcoinTest : MonoBehaviour
+{
+    [UsedImplicitly] void Start()
+    {
+        Debug.Log( "Hello World! " + new Key().GetWif( NBitcoin.Network.Main ) );
+    }
+}
