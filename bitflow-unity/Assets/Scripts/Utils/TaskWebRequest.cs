d5442c20ebdc7b890bd0a375f3d3744e42b2ac15
Abel Dantas <dantas.abel@gmail.com>
2019-05-04 14:18:01 +0100

    Add utility to make web requests

---
+++ bitflow-unity/Assets/Scripts/Utils/TaskWebRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+public static class TaskWebRequest
+{
+    public static async Task<string> Get(string uri)
+    {
+        var result = "";
+        HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(uri);
+        using (WebResponse myResponse = myRequest.GetResponse())
+        {
+            using (StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8))
+            {
+                result = sr.ReadToEnd();
+            }
+        }
+
+        return result;
+    }
+}
