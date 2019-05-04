341007d0cd7dcf106b9fe04f1a65ca04c63fe447
Abel Dantas <dantas.abel@gmail.com>
2019-05-04 14:19:39 +0100

    Test NBitcoin

---
+++ bitflow-unity/Assets/Scripts/GenerateWallet.cs
@@ -0,0 +1,12 @@
+﻿using NBitcoin;
+using UnityEngine;
+
+public class GenerateWallet : MonoBehaviour
+{
+    void Start()
+    {
+        var privateKey = new Key(); // generate a random private key
+        var publicKey = privateKey.PubKey;
+        Debug.Log(publicKey); // 0251036303164f6c458e9f7abecb4e55e5ce9ec2b2f1d06d633c9653a07976560c
+    }
+}
