1312ec08090d19012fbafd6a996003464240223c
Abel Dantas <dantas.abel@gmail.com>
2019-05-05 15:41:17 +0100

    Add UI schema form builder

---
+++ bitflow-unity/Assets/Bitflow/Scripts/UI/SchemaUIController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[System.Serializable] public class SubmissionEvent : UnityEvent<Dictionary<string, object>>
+{
+}
+
+public class SchemaUIController : MonoBehaviour
+{
+    [SerializeField] Transform Parent;
+
+    [Space(10)]
+    [SerializeField] GameObject TextInputFieldPrefab;
+    [SerializeField] GameObject SchemaNameTextPrefab;
+    [SerializeField] GameObject NumberInputFieldPrefab;
+    [SerializeField] GameObject ImageUploadWidgetPrefab;
+    [SerializeField] GameObject FileUploadWidgetPrefab;
+
+    [SerializeField] GameObject ButtonPrefab;
+
+    public SubmissionEvent OnSubmit;
+
+    Dictionary<string, ISchemaFieldGetter> FieldGetterMap = new Dictionary<string, ISchemaFieldGetter>();
+
+    public void Build( Schema schema )
+    {
+        // Instanciar tudo
+        // No fim de instanciar tudo, o field getter map vai estar populado
+
+        var button = Instantiate( ButtonPrefab, Parent );
+        var b = button.GetComponent<Button>();
+        b.onClick.AddListener( Submit );
+    }
+
+    void Submit()
+    {
+        var submitMap = new Dictionary<string, object>();
+        foreach ( var element in FieldGetterMap )
+        {
+            submitMap.Add( element.Key, element.Value.Get() );
+        }
+
+        OnSubmit.Invoke( submitMap );
+    }
+}
---
+++ bitflow-unity/Assets/Bitflow/Scripts/UI/TestSchema.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class TestSchema : MonoBehaviour
+{
+    [SerializeField] SchemaUIController Controller;
+
+    public void Start()
+    {
+        Controller.Build( new Schema
+        {
+            Id = "0",
+            Name = "Collect Name",
+            Fields =
+            {
+                new Schema.Types.Field
+                {
+                    Key = "0",
+                    Label = "First Name",
+                    Type = Schema.Types.Type.Text
+                },
+                new Schema.Types.Field
+                {
+                    Key = "1",
+                    Label = "Surname",
+                    Type = Schema.Types.Type.Text
+                }
+            }
+        } );
+        Controller.OnSubmit.AddListener( BuildAndBroadcastTransaction );
+    }
+
+    public void BuildAndBroadcastTransaction(Dictionary<string, object> submitMap)
+    {
+        Debug.Log( "Submit the map: " + JsonConvert.SerializeObject( submitMap ) );
+    }
+}
