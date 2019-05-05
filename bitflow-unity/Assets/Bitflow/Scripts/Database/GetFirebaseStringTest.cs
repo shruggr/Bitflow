0a969ecac7670b3d71f4bd99edbfa3dbee8da413
Abel Dantas <dantas.abel@gmail.com>
2019-05-05 04:45:50 +0100

    Adapt test class to new definition

--- bitflow-unity/Assets/Bitflow/Scripts/Database/GetFirebaseStringTest.cs
+++ bitflow-unity/Assets/Bitflow/Scripts/Database/GetFirebaseStringTest.cs
@@ -11,82 +11,18 @@ public class GetFirebaseStringTest : MonoBehaviour
         {
             await Task.Delay( 100 );
         }
-        Debug.Log("Firebase initialized");
 
-        var worflowState = new WorkflowState
+        Debug.Log( "Firebase initialized" );
+
+        var worflowState = new Workflow
         {
-            Id = "yadadadada",
-            State = "same-old-state",
-            WorkflowTxn = "AnAwesomeWorkflowTxn",
-            Status = WorkflowState.Types.Status.Error,
-            Tasks =
-            {
-                new WorkflowState.Types.Task
-                {
-                    Step = new Step
-                    {
-                        FundsRequired = 100000,
-                        Name = "someguy",
-                        Payee = "someOtherGuy",
-                        SchemaTxn = "ComplicatedJsonWithABunchOfStuff",
-                        ValidationScriptTxn = ""
-                    },
-                    Status = WorkflowState.Types.Status.Error,
-                    Utxo = new WorkflowState.Types.UTXO
-                    {
-                        TxId = "myId",
-                        Vout = 0,
-                        Address = "some guy",
-                        Script = "op return stuff",
-                        Satoshis = 1131283
-                    }
-                },
-                new WorkflowState.Types.Task
-                {
-                    Step = new Step
-                    {
-                        FundsRequired = 100000,
-                        Name = "someguy",
-                        Payee = "someOtherGuy",
-                        SchemaTxn = "ComplicatedJsonWithABunchOfStuff",
-                        ValidationScriptTxn = ""
-                    },
-                    Status = WorkflowState.Types.Status.Error,
-                    Utxo = new WorkflowState.Types.UTXO
-                    {
-                        TxId = "myId",
-                        Vout = 0,
-                        Address = "some guy",
-                        Script = "op return stuff",
-                        Satoshis = 1131283
-                    }
-                },
-                new WorkflowState.Types.Task
-                {
-                    Step = new Step
-                    {
-                        FundsRequired = 100000,
-                        Name = "someguy",
-                        Payee = "someOtherGuy",
-                        SchemaTxn = "ComplicatedJsonWithABunchOfStuff",
-                        ValidationScriptTxn = ""
-                    },
-                    Status = WorkflowState.Types.Status.Error,
-                    Utxo = new WorkflowState.Types.UTXO
-                    {
-                        TxId = "myId",
-                        Vout = 0,
-                        Address = "some guy",
-                        Script = "op return stuff",
-                        Satoshis = 1131283
-                    }
-                }
-            }
+            Txid = "Test"
         };
 
-        await FirebaseManager.Instance.Ref.Child( "DummyWorkflowState" ).SetRawJsonValueAsync( JsonConvert.SerializeObject( worflowState ) );
+        await FirebaseManager.Instance.Ref.Child( "DummyWorkflowState" )
+            .SetRawJsonValueAsync( JsonConvert.SerializeObject( worflowState ) );
 
-        var value = await FirebaseHelper.Get<WorkflowState>( FirebaseManager.Instance.Ref.Child("DummyWorkflowState") );
+        var value = await FirebaseHelper.Get<Workflow>( FirebaseManager.Instance.Ref.Child( "DummyWorkflowState" ) );
         Debug.Log( JsonConvert.SerializeObject( value ) );
     }
 }
