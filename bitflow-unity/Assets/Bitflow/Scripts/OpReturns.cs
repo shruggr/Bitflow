f56fcf7ef9501b0b1fa6dc2ffe1e80777de15b97
Abel Dantas <dantas.abel@gmail.com>
2019-05-05 06:41:20 +0100

    Build op return for image upload via B://

---
+++ bitflow-unity/Assets/Bitflow/Scripts/OpReturns.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+public static class OpReturns
+{
+    const string BitcoinSimpleStorageProtocol = "19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut";
+
+    public static byte[][] MakeImg( byte[] buffer )
+    {
+        return new[]
+        {
+            Encoding.UTF8.GetBytes( BitcoinSimpleStorageProtocol ),
+            buffer,
+            Encoding.UTF8.GetBytes( "image/png" ),
+            Encoding.UTF8.GetBytes( "binary" ),
+        };
+    }
+}
