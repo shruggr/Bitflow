41b192c04547f2dddf84b06e62cadb781cef7e19
Abel Dantas <dantas.abel@gmail.com>
2019-05-05 11:16:32 +0100

    Add some feedback when uploading image

--- bitflow-unity/Assets/Bitflow/Scripts/Camera/TakePicture.cs
+++ bitflow-unity/Assets/Bitflow/Scripts/Camera/TakePicture.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 using UnityEngine;
@@ -6,62 +7,80 @@ using UnityEngine.UI;
 
 public class TakePicture : MonoBehaviour
 {
-    [SerializeField] Button Button;
-
     public void Take()
     {
-        Button.gameObject.SetActive( false );
         NativeToolkit.OnCameraShotComplete += OnCameraShotComplete;
         NativeToolkit.TakeCameraShot();
     }
 
     void OnCameraShotComplete( Texture2D texture2D, string path )
     {
-        var pix = texture2D.GetPixels32();
-
-        // Copy the reversed image data to a new texture.
-        var tex = new Texture2D( texture2D.width, texture2D.height );
-        tex.SetPixels32( pix );
-        tex.Apply();
-
-        // TODO: Duplicated code with UploadImage.cs
-        var width = 0;
-        var height = 0;
-        var maxSize = 1024;
-        if ( tex.width > tex.height )
+        ModalDialog.Instance.Show( "Processing Photo",
+            "Please wait while your photo is encoded into a txn and broadcast" );
+        try
         {
-            if ( tex.width > maxSize )
+            var pix = texture2D.GetPixels32();
+
+            // Copy the reversed image data to a new texture.
+            var tex = new Texture2D( texture2D.width, texture2D.height );
+            tex.SetPixels32( pix );
+            tex.Apply();
+
+            // TODO: Duplicated code with UploadImage.cs
+            var width = 0;
+            var height = 0;
+            var maxSize = 800;
+            if ( tex.width > tex.height )
             {
-                width = maxSize;
-                height = ( tex.height * maxSize ) / tex.width;
+                if ( tex.width > maxSize )
+                {
+                    width = maxSize;
+                    height = ( tex.height * maxSize ) / tex.width;
+                }
             }
-        }
-        else
-        {
-            if ( tex.height > maxSize )
+            else
             {
-                height = maxSize;
-                width = ( tex.width * maxSize ) / tex.height;
+                if ( tex.height > maxSize )
+                {
+                    height = maxSize;
+                    width = ( tex.width * maxSize ) / tex.height;
+                }
             }
+
+            TextureScale.Point( tex, width, height );
+
+            Debug.Log( $"Texture is {tex.width}x{tex.height}" );
+
+            var buffer = tex.EncodeToJPG();
+            Debug.Log( buffer.Length );
+
+            BitIndexUtils.QueryUtxos( Authenticator.Instance.Identity.Address, utxos =>
+            {
+                var utxo = utxos.First();
+                Debug.Log( JsonConvert.SerializeObject( utxo ) );
+
+                UtxoUtils.BuildTxnFromUtxo(
+                        Authenticator.Instance.Identity.PrivateKey,
+                        utxo.ToUTXO(),
+                        OpReturns.MakeImg( buffer ) )
+                    .Spend( txn =>
+                    {
+                        Debug.Log( $"https://bico.media/{txn}" );
+                        ModalDialog.Instance.Hide();
+                        ModalDialog.Instance.Show( "Image successfully uploaded",
+                            $"Check it out at: https://bico.media/{txn}", "Copy to Clipboard", "Ok" );
+                        ModalDialog.Instance.CallbackYes.AddListener( () =>
+                        {
+                            UniClipboard.SetText( $"https://bico.media/{txn}" );
+                        } );
+                    } );
+            } );
         }
-
-        TextureScale.Point( tex, width, height );
-
-        Debug.Log( $"Texture is {tex.width}x{tex.height}" );
-
-        var buffer = tex.EncodeToJPG();
-        Debug.Log( buffer.Length );
-
-        BitIndexUtils.QueryUtxos( Authenticator.Instance.Identity.Address, utxos =>
+        catch ( Exception e )
         {
-            var utxo = utxos.First();
-            Debug.Log( JsonConvert.SerializeObject( utxo ) );
-
-            UtxoUtils.BuildTxnFromUtxo(
-                    Authenticator.Instance.Identity.PrivateKey,
-                    utxo.ToUTXO(),
-                    OpReturns.MakeImg( buffer ) )
-                .Spend( txn => { Debug.Log( $"https://bico.media/{txn}" ); } );
-        } );
+            ModalDialog.Instance.Hide();
+            ModalDialog.Instance.Show( $"Something went wrong, try again later",
+                $"{e.GetType()}: {e.Message}", "Ok" );
+        }
     }
 }
