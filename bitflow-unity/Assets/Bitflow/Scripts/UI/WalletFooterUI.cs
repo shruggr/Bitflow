7a8b9b5a34b7ec5b9dab555e76f99cc919fd5cb2
Abel Dantas <dantas.abel@gmail.com>
2019-05-05 21:53:33 +0100

    Add wallet addr and funds to main screen

---
+++ bitflow-unity/Assets/Bitflow/Scripts/UI/WalletFooterUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WalletFooterUI : MonoBehaviour
+{
+    [SerializeField] Text WalletAddress;
+    [SerializeField] Text WalletFunds;
+
+    async void Start()
+    {
+        while ( Authenticator.Instance == null || Authenticator.Instance.Identity == null )
+        {
+            await Task.Delay( 200 );
+        }
+
+        WalletAddress.text = $"Addr: {Authenticator.Instance.Identity.Address}";
+        BitIndexUtils.QueryAddressDetails( Authenticator.Instance.Identity.Address,
+            details => { WalletFunds.text = $"Funds: {details.Balance.ToString()} BSV"; } );
+    }
+}
