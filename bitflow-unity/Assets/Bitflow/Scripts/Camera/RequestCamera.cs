7298555797c363cbd4fac43be1fffe3f33957e01
Abel Dantas <dantas.abel@gmail.com>
2019-05-05 04:41:49 +0100

    Add intro scene to request permissions

---
+++ bitflow-unity/Assets/Bitflow/Scripts/Camera/RequestCamera.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+
+#endif
+
+public class RequestCamera : MonoBehaviour
+{
+    [SerializeField] GameObject AskForCameraPanel;
+
+    void Start()
+    {
+#if PLATFORM_ANDROID
+        if ( Permission.HasUserAuthorizedPermission( Permission.Camera ) &&
+             Permission.HasUserAuthorizedPermission( Permission.ExternalStorageWrite ) )
+        {
+            AskForCameraPanel.SetActive( false );
+            SceneManager.LoadScene( "Main" );
+        }
+        else
+        {
+            RequestPermission();
+        }
+#endif
+    }
+
+    void Update()
+    {
+#if PLATFORM_ANDROID
+        if ( Permission.HasUserAuthorizedPermission( Permission.Camera ) &&
+             Permission.HasUserAuthorizedPermission( Permission.ExternalStorageWrite ) )
+        {
+            AskForCameraPanel.SetActive( false );
+            SceneManager.LoadScene( "Main" );
+        }
+        else
+        {
+            AskForCameraPanel.SetActive( true );
+        }
+#endif
+    }
+
+    public void RequestPermission()
+    {
+        Debug.Log("OnRequestPermission");
+        if ( !Permission.HasUserAuthorizedPermission( Permission.Camera ) )
+        {
+            Debug.Log("Does not have camera");
+            Permission.RequestUserPermission( Permission.Camera );
+        }
+        else
+        {
+            Debug.Log("Has Camera");
+        }
+
+        if ( !Permission.HasUserAuthorizedPermission( Permission.ExternalStorageWrite ) )
+        {
+            Debug.Log("Does not have external storage");
+            Permission.RequestUserPermission( Permission.ExternalStorageWrite );
+        }
+        else
+        {
+            Debug.Log("Has external storage");
+        }
+    }
+
+    public void OpenSettings()
+    {
+        NativeCamera.OpenSettings();
+    }
+}
---
+++ bitflow-unity/Assets/Bitflow/Scripts/Camera/TakePicture.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TakePicture : MonoBehaviour
+{
+    [SerializeField] Button Button;
+
+    public void Take()
+    {
+        Button.gameObject.SetActive( false );
+        NativeToolkit.OnCameraShotComplete += OnCameraShotComplete;
+        NativeToolkit.TakeCameraShot();
+    }
+
+    void OnCameraShotComplete( Texture2D texture2D, string path )
+    {
+        var img = texture2D.Resize( 250, 250 );
+        if ( img )
+        {
+            Debug.Log( "Resize successful" );
+        }
+        Debug.Log( path );
+        Debug.Log( $"width: {texture2D.width}, height: {texture2D.height}" );
+
+        var buffer = texture2D.GetRawTextureData();
+        Debug.Log( $"buffer length: {buffer.Length}" );
+
+        var encodedStr = Encoding.UTF8.GetString( buffer, 0, buffer.Length );
+    }
+}
