ab7de25796abd7f72efce6abfbbf558859ba8a3b
Abel Dantas <dantas.abel@gmail.com>
2019-05-05 11:15:22 +0100

    Lay out some sort of modular UI - mainly popups

---
+++ bitflow-unity/Assets/Bitflow/Scripts/UI/ModalDialog.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ModalDialog : MonoBehaviour {
+	public static ModalDialog Instance;
+
+	public UnityEvent CallbackYes;
+	public UnityEvent CallbackNo;
+	public Text ModalTitle;
+	public Text ModalMessage;
+	public GameObject NoButton;
+    public Text NoButtonLabel;
+	public GameObject YesButton;
+    public Text YesButtonLabel;
+    public GameObject Container;
+    public GameObject Loading;
+	public Animator Animator;
+
+	void Awake() {
+		Instance = this;
+		Container.SetActive(false);
+	}
+	
+	public void Show(string title, string message, string yesButton = "", string noButton = "") {
+		Container.SetActive(true);
+		ModalTitle.text = title;
+		ModalMessage.text = message;
+		YesButton.SetActive(!string.IsNullOrEmpty(yesButton));
+	    YesButtonLabel.text = yesButton;
+	    NoButton.SetActive(!string.IsNullOrEmpty(noButton));
+	    NoButtonLabel.text = noButton;
+        Loading.SetActive( string.IsNullOrEmpty( yesButton ) && string.IsNullOrEmpty( noButton ) );
+        Animator.Play("Fade-in");
+	}
+
+	public void Hide() {
+		Container.SetActive(false);
+		Animator.Play("Fade-out");
+	}
+
+    public void OnYes()
+    {
+        CallbackYes.Invoke();
+        Hide();
+    }
+
+    public void OnNo()
+    {
+        CallbackNo.Invoke();
+        Hide();
+    }
+
+    public bool IsVisible()
+    {
+        return Container.activeSelf;
+    }
+}
--- bitflow-unity/Assets/Libraries/muip/Scripts/ModalByScript.cs
+++ bitflow-unity/Assets/Libraries/muip/Scripts/ModalByScript.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class ModalByScript : MonoBehaviour {
 	public void DisplayModal() {
-		ModalDialog.Instance.Show("Test", "Message", true);
+		//ModalDialog.Instance.Show("Test", "Message", true);
 	}
 }
